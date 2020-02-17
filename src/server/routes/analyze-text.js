ac9aaf2c8344aae00997a9c552c327cfcd9992ca
Carlos Loureda <balanzeneto@gmail.com>
2020-02-17 12:37:30 +0100

    temp

--- src/server/routes/analyze-text.js
+++ src/server/routes/analyze-text.js
@@ -7,10 +7,20 @@ const analyzeText = (req, res) => {
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   }
-  const { text } = req.body;
+  const { text } = req.parms;
   //   TODO: Do the endpoint calls and fetch response ..
 
   console.log("[/analyze-text] endpoint called with `text`: ", text);
+  console.log("req.parms: ", req.parms);
+  textapi.sentiment(text, (error, response) => {
+    try {
+      if (error === null) {
+        console.log("response: ", response);
+      }
+    } catch (e) {
+      console.log("error: ", e);
+    }
+  });
 
   res.status(200).send("Hello from the [/analyze-text] endpoint!");
   //   res.status(200).send("User entry properly saved");
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,21 +1,36 @@
 const router = require("express").Router();
 const { check } = require("express-validator");
-
+const textapi = require("../api/textapi");
+const { validationResult } = require("express-validator");
 router.get("/", (req, res) => {
   console.log("[/] endpoint called");
   res.send("Hello from the server!");
 });
-
+const analyzeText = require("./analyze-text");
 // req should have a text file
-router.get(
-  "/analyze-text",
-  [
-    check("text").isString(),
-    check("text")
-      .not()
-      .isEmpty()
-  ],
-  require("./analyze-text")
-);
+router.get("/analyze-text", (req, res) => {
+  // Validate data
+  //   const errors = validationResult(req);
+  //   if (!errors.isEmpty()) {
+  //     return res.status(422).json({ errors: errors.array() });
+  //   }
+  const { text } = req.params;
+  //   TODO: Do the endpoint calls and fetch response ..
+
+  console.log("[/analyze-text] endpoint called with `text`: ", text);
+  console.log("req.parms: ", req.parms);
+  textapi.sentiment(text, (error, response) => {
+    try {
+      if (error === null) {
+        console.log("response: ", response);
+      }
+    } catch (e) {
+      console.log("error: ", e);
+    }
+  });
+
+  res.status(200).send("Hello from the [/analyze-text] endpoint!");
+  //   res.status(200).send("User entry properly saved");
+});
 
 module.exports = router;
