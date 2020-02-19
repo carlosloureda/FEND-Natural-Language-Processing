5c813b92c3aa48ff1619c3830ce1bfa1709d0b0a
Carlos Loureda <balanzeneto@gmail.com>
2020-02-19 03:49:00 +0100

    [feat] Added all the other responses from text api

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -51,6 +51,100 @@ const populateSentimentUI = sentimentData => {
     subjectivity_confidence * 100
   );
 };
+
+/**
+ * Populates UI with the data for hashtags
+ * @param {array} hashtags - object with all the hashtags
+ */
+const populateHashtagsUI = hashtags => {
+  const hashtagsWrapper = document.getElementById("hastags");
+  if (!hashtags.length) {
+    // TODO: add some styling to this
+    hashtagsWrapper.innerText = "No hashtags found for this text ...";
+    return;
+  }
+
+  hashtags.forEach(hashtag => {
+    const el = document.createElement("span");
+    el.classList = "chip";
+    el.innerText = hashtag;
+    hashtagsWrapper.appendChild(el);
+  });
+};
+
+/**
+ * Populates UI with the data for categories
+ * @param {array} categories - array with all the categories
+ */
+const populateCategoriesUI = categories => {
+  const categoriesWrapper = document.getElementById("categories");
+  if (!categories.length) {
+    // TODO: add some styling to this
+    categoriesWrapper.innerText = "No categories found for this text ...";
+    return;
+  }
+
+  categories.forEach(hashtag => {
+    const el = document.createElement("span");
+    el.classList = "chip";
+    el.innerText = hashtag;
+    categoriesWrapper.appendChild(el);
+  });
+};
+
+/**
+ * Populates UI with the data for summary
+ * @param {object:{text, sentences}} categories - object with all the summary
+ */
+const populateSummaryUI = summary => {
+  const { text, sentences } = summary;
+
+  const summaryWrapper = document.getElementById("summary");
+  if (sentences) {
+    sentences.forEach(sentence => {
+      const el = document.createElement("p");
+      // el.classList = "chip";
+      el.innerText = sentence;
+      summaryWrapper.appendChild(el);
+    });
+  } else {
+    const el = document.createElement("p");
+    summaryWrapper.appendChild(el);
+  }
+};
+
+/**
+ * Populates UI with the data for extract
+ * @param {object:{author, feeds, video}} extract - object with all the extract
+ */
+const populateExtractUI = extract => {
+  const { author, feeds, videos } = extract;
+
+  document.getElementById("author").innerText = author;
+
+  const feedsWrapper = document.getElementById("feeds");
+  feeds.forEach(feed => {
+    const el = document.createElement("p");
+    const anchor = document.createElement("a");
+    anchor.href = feed;
+    anchor.innerText = feed;
+    el.appendChild(anchor);
+    feedsWrapper.appendChild(el);
+  });
+
+  const videosWrapper = document.getElementById("videos");
+  videos.forEach(video => {
+    const iframe = document.createElement("iframe");
+    iframe.classList = "video-iframe";
+    iframe.src = video;
+    videosWrapper.appendChild(iframe);
+  });
+};
+
+/**
+ * Handles all the events related to the form submission
+ * @param {event} e - The event fired on click on the form submit
+ */
 export const fromHandler = async e => {
   e.preventDefault();
   //   TODO: check text and url
@@ -61,6 +155,18 @@ export const fromHandler = async e => {
   if (info) {
     if (info.sentiment) {
       populateSentimentUI(info.sentiment);
+    }
+    if (info.categories) {
+      populateCategoriesUI(info.categories.categories);
+    }
+    if (info.hashtags) {
+      populateHashtagsUI(info.hashtags.hashtags);
+    }
+    if (info.summary) {
+      populateSummaryUI(info.summary);
+    }
+    if (info.extract) {
+      populateExtractUI(info.extract);
     }
   }
   console.log("Form Hanlder called: ", info);
