a8c2cb2d0a5e6cc8eb2b69fe29af3461187ce79d
Carlos Loureda <balanzeneto@gmail.com>
2020-02-19 20:26:05 +0100

    [feat] Added tests for instructions.js and modalHandler.js

--- src/client/js/modalHandler.js
+++ src/client/js/modalHandler.js
@@ -10,21 +10,32 @@ export const openErrorModal = errorMessage => {
 };
 
 /**
+ * Handles When the user clicks on <span> (x), close the modal
+ */
+const clearModalContent = () => {
+  const modal = document.getElementById("errorModal");
+  document.getElementById("modal-body-content").innerHTML = "";
+  modal.style.display = "none";
+};
+
+/**
+ * Handles when the user clicks anywhere outside of the modal, close it
+ * @param {event} event - The event that triggered the event listener
+ */
+const closeModal = event => {
+  const modal = document.getElementById("errorModal");
+  if (event.target == modal) {
+    document.getElementById("modal-body-content").innerHTML = "";
+    modal.style.display = "none";
+  }
+};
+/**
  * Hanlder for adding event listeners for closing modal window
  */
 export const errorModalHandler = () => {
-  let modal = document.getElementById("errorModal");
   let span = document.querySelector(".modal-close");
   // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    document.getElementById("modal-body-content").innerHTML = "";
-    modal.style.display = "none";
-  };
+  span.onclick = clearModalContent;
   // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      document.getElementById("modal-body-content").innerHTML = "";
-      modal.style.display = "none";
-    }
-  };
+  window.onclick = closeModal;
 };
